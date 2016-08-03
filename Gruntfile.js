2f3f3f10332dea17176b5094365c8ed6565db176
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-08-02 22:42:05 -0300

    Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
       options: {
         urls: ['http://www.fa7.edu.br'],
         key: 'AIzaSyAFMZf07E4WoJSRygfwvqO4ClF7Mfxa4VY',
-        reports: ['$CIRCLE_TEST_REPORTS/junit/gpsi-home.xml']
+        reports: [process.env.CIRCLE_TEST_REPORTS + '/junit/gpsi-home.xml']
       }
     }
   });
