59ff2ddd7bbbfb73a056e0b1cdd8c7a18b64f782
Carlos Mendes Aderaldo <cmendesce@gmail.com>
2016-08-03 16:50:37 -0300

    Add new pages to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pagespeed_junit: {
       options: {
-        urls: ['http://www.fa7.edu.br'],
+        urls: ['http://www.fa7.edu.br', 'http://www.fa7.edu.br/graduacao', 'http://www.fa7.edu.br/direito'],
         key: 'AIzaSyAFMZf07E4WoJSRygfwvqO4ClF7Mfxa4VY',
         reports: [process.env.CIRCLE_TEST_REPORTS + '/junit/gpsi-home.xml']
       }
